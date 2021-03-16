112e8caf5108f6dfd548c5ced25d203e06a229c1
Andreas Weibye <13300393+Weibye@users.noreply.github.com>
2021-03-16 08:20:47 +0100

    More testing

--- src/main.ts
+++ src/main.ts
@@ -10,13 +10,15 @@ import { GetExamplesFromReadme } from './ReadmeExamples';
 // import { wait } from './wait'
 
 // Make sure all source files
+let source: string;
+let targets: string[];
 
 // Config
-let sourcePath = 'examples/';
+const sourcePathDefault = 'examples/';
 const whiteListFileTypes = ['.rs']; // If this is empty, look for all files
-const targetsPaths = ['__tests__/testData/Cargo.toml', '__tests__/testData/examples/README.md'];
+const targetPathDefault = ['__tests__/testData/Cargo.toml', '__tests__/testData/examples/README.md'];
 
-const pathToReadme = `${sourcePath}README.md`;
+// const pathToReadme = `${sourcePath}README.md`;
 const pathToCargo = 'Cargo.toml';
 
 const foldersToExclude = ['__tests__/testData/examples/ios/', '__tests__/testData/examples/excludefolder/'];
@@ -28,78 +30,90 @@ const checkCargo = true;
 
 async function run(): Promise<void> {
     try {
-        console.log('======= Starting Job =======');
+        console.log('======= Getting inputs =======');
+        const sourceInput = core.getInput('source');
+        if (sourceInput === undefined || sourceInput === '') {
+            source = sourcePathDefault;
+        } else {
+            source = sourceInput;
+        }
+        console.log(`Source: ${source}`);
 
-        console.log('Getting input');
-        
-        sourcePath = core.getInput('source_path');
-        console.log(`Source Path: ${sourcePath}`);
-        console.log(`Target Path: ${core.getInput('target_paths')}`);
-        const targets = JSON.parse(core.getInput('target_paths'));
+        const targetsInput = core.getInput('targets');
+        if (targetsInput === undefined || targetsInput === '') {
+            targets = targetPathDefault;
+        } else {
+            targets = JSON.parse(targetsInput);
+        }
         console.log(`Targets: ${targets}`);
 
-        console.log('======= Starting Job =======');
+        // console.log(`Target Path: ${core.getInput('target_paths')}`);
+        // const targets = JSON.parse(core.getInput('target_paths'));
+        // console.log(`Targets: ${targets}`);
+        return;
 
-        if (!checkCargo && !checkReadme) {
-            core.setFailed('Error with configuration: Both Cargo and README checks are disabled meaning this script will do nothing. At least one should be enabled');
-            return;
-        }
+        // console.log('======= Starting Job =======');
 
-        // Check that the example directory exists
-        if (!IsValidPath(sourcePath)) {
-            core.setFailed(`Examples directory not found: ${sourcePath}`);
-            return;
-        }
+        // if (!checkCargo && !checkReadme) {
+        //     core.setFailed('Error with configuration: Both Cargo and README checks are disabled meaning this script will do nothing. At least one should be enabled');
+        //     return;
+        // }
 
-        // Check that the Cargo.toml exist
-        if (checkCargo && !IsValidPath(targetsPaths[0])) {
-            core.setFailed(`Cargo.toml not found: ${targetsPaths[0]}`);
-            return;
-        }
+        // // Check that the example directory exists
+        // if (!IsValidPath(sourcePath)) {
+        //     core.setFailed(`Examples directory not found: ${sourcePath}`);
+        //     return;
+        // }
 
-        // Check that the readme exists
-        if (checkReadme && !IsValidPath(targetsPaths[1])) {
-            core.setFailed(`Examples README not found: ${targetsPaths[1]}`);
-            return;
-        }
+        // // Check that the Cargo.toml exist
+        // if (checkCargo && !IsValidPath(targetsPaths[0])) {
+        //     core.setFailed(`Cargo.toml not found: ${targetsPaths[0]}`);
+        //     return;
+        // }
 
-        // Collect the data from the various sources and normalize them for comparison
+        // // Check that the readme exists
+        // if (checkReadme && !IsValidPath(targetsPaths[1])) {
+        //     core.setFailed(`Examples README not found: ${targetsPaths[1]}`);
+        //     return;
+        // }
 
-        // Get examples from directories
-        console.log('======= DISK =======');
-        const diskExamples: FileData[] = GetExamplesFromDisk(sourcePath, filesToExclude, foldersToExclude);
-        if (diskExamples.length > 0) {
-            console.log(`Found ${diskExamples.length} examples in ${sourcePath}`);
-            // for (const example of diskExamples) {
-            //     console.log(example);
-            // }
-        } else {
-            core.setFailed('Found no examples on disk');
-        }
+        // // Collect the data from the various sources and normalize them for comparison
 
-        // Get examples listed in the Cargo.toml
-        console.log('======= CARGO =======');
-        const cargoExamples: FileData[] = checkCargo ? GetExamplesFromCargo(targetsPaths[0], filesToExclude, foldersToExclude) : [];
-        if (cargoExamples.length > 0) {
-            console.log(`Found ${cargoExamples.length} examples in ${pathToCargo}`);
-            // for (const example of cargoExamples) {
-            //     console.log(example);
-            // }
-        } else {
-            if (checkCargo) core.setFailed('Found no examples in Cargo.toml');
-        }
+        // // Get examples from directories
+        // console.log('======= DISK =======');
+        // const diskExamples: FileData[] = GetExamplesFromDisk(sourcePath, filesToExclude, foldersToExclude);
+        // if (diskExamples.length > 0) {
+        //     console.log(`Found ${diskExamples.length} examples in ${sourcePath}`);
+        //     // for (const example of diskExamples) {
+        //     //     console.log(example);
+        //     // }
+        // } else {
+        //     core.setFailed('Found no examples on disk');
+        // }
 
-        // Get examples listed in the README
-        console.log('======= README =======');
-        const readmeExamples: FileData[] = checkReadme ? GetExamplesFromReadme(targetsPaths[1], filesToExclude, foldersToExclude) : [];
-        if (readmeExamples.length > 0) {
-            console.log(`Found ${readmeExamples.length} examples in ${pathToReadme}`);
-            // for (const example of readmeExamples) {
-            //     console.log(example);
-            // }
-        } else {
-            if (checkReadme) core.setFailed('Found no examples in README');
-        }
+        // // Get examples listed in the Cargo.toml
+        // console.log('======= CARGO =======');
+        // const cargoExamples: FileData[] = checkCargo ? GetExamplesFromCargo(targetsPaths[0], filesToExclude, foldersToExclude) : [];
+        // if (cargoExamples.length > 0) {
+        //     console.log(`Found ${cargoExamples.length} examples in ${pathToCargo}`);
+        //     // for (const example of cargoExamples) {
+        //     //     console.log(example);
+        //     // }
+        // } else {
+        //     if (checkCargo) core.setFailed('Found no examples in Cargo.toml');
+        // }
+
+        // // Get examples listed in the README
+        // console.log('======= README =======');
+        // const readmeExamples: FileData[] = checkReadme ? GetExamplesFromReadme(targetsPaths[1], filesToExclude, foldersToExclude) : [];
+        // if (readmeExamples.length > 0) {
+        //     console.log(`Found ${readmeExamples.length} examples in ${pathToReadme}`);
+        //     // for (const example of readmeExamples) {
+        //     //     console.log(example);
+        //     // }
+        // } else {
+        //     if (checkReadme) core.setFailed('Found no examples in README');
+        // }
 
         // console.log("======= Cross referencing issues =======");
         // const issues = CrossReference(diskExamples, cargoExamples, readmeExamples);
