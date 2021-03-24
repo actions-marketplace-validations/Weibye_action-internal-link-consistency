aa2361a6a9e6cfe5f4a2ac16e66e874be9e799c8
Andreas Weibye <13300393+Weibye@users.noreply.github.com>
2021-03-24 22:00:02 +0100

    Adding vscode setup to gitignore

--- src/main.ts
+++ src/main.ts
@@ -16,14 +16,10 @@ async function run(): Promise<void> {
         const config = new Setup().Config;
         console.log(`Running job with config: \n${config.ToString()}`);
 
-        // Get examples from directories
         console.log('======= Getting source data =======');
         const sourceData: FileDetails[] = GetSourceData(config.Source, config);
         if (sourceData.length > 0) {
             console.log(`Found ${sourceData.length} entries in ${config.Source}`);
-            // for (const data of sourceData) {
-            //     console.log(data);
-            // }
         } else {
             core.setFailed('Found no entries in source');
         }
@@ -41,38 +37,23 @@ async function run(): Promise<void> {
         console.log("======= Cross referencing issues =======");
         const crossChecker = new CrossReferencer(sourceData, targetData);
         if (crossChecker.HasIssues) {
-            if (crossChecker.NotInAnyTarget.length > 0) {
-                console.log(`+++++++++++\nFollowing files in source was not found in any targets:\n`);
-                crossChecker.NotInAnyTarget.forEach(issue => {
-                    console.log(`=>\tFile: ${issue.Path}\n`);
-                });
-                console.log('Make sure they are added to the sources, or ignored from the search');
-            }
-
-            if (crossChecker.NotInAllTargets.length > 0) {
-                console.log(`+++++++++++\nFollowing files in source was found in some but not all targets:\n`);
-                crossChecker.NotInAllTargets.forEach(issue => {
+            if (crossChecker.MissingFromTargets.length > 0) {
+                console.log(`Following files in source was not found in the following target(s):\n`);
+                crossChecker.MissingFromTargets.forEach(issue => {
                     console.log(`=>\tFile: ${issue.Path} \n\tMissing from targets: ${issue.MissingTargets}\n`);
                 });
-                console.log('Please add them to the remaining targets');
+                console.log('Please add them to the remaining targets or remove them from disk\n');
             }
-
-            if (crossChecker.NotInSource.length > 0) {
-                console.log(`+++++++++++\nFollowing links found in targets but could not find corresponding file in source:\n`);
-                crossChecker.NotInSource.forEach(issue => {
+            if (crossChecker.MissingFromSource.length > 0) {
+                console.log(`Following links was found in target(s) but could not find corresponding file in source:\n`);
+                crossChecker.MissingFromSource.forEach(issue => {
                     console.log(`=>\tFile: ${issue.Path} \n\tListed in target: ${issue.InTarget}\n`);
                 });
-                console.log('Please remove them from the target or make sure the link points to the correct file');
+                console.log('Please remove them from the target or make sure the link points to the correct file.');
+                console.log('Note: This often might indicate a typo in the link.\n')
             }
-            core.setFailed('Cross referencing found issues, see output log to deal with them');
+            core.setFailed('Cross referencing found issues, see output log to fix them');
         }
-
-        // issues.diskIssues.forEach(issue => {
-        //     console.log(`File on disk not in Target: ${issue.Path}`);
-        // });
-        // issues.targetIssues.forEach(issue => {
-        //     console.log(`Link in target not found on disk: \nExpected file: ${issue.Path}\nMatch in target: ${issue.OriginalMatch}\nTarget: ${issue.Target}`);
-        // });
     } catch (error) {
         core.setFailed(error.message);
     }
