88f152af20a167f41e98e32c81a99f2c6b9bdef6
Andreas Weibye <13300393+Weibye@users.noreply.github.com>
2021-03-24 21:59:08 +0100

    Updating defaults to be more correct

--- src/Defaults.ts
+++ src/Defaults.ts
@@ -3,8 +3,8 @@ import { LinkStyle } from './LinkStyle';
 
 export const defaultSource = './__tests__/testData/examples/';
 export const defaultFileTypes = ['rs'];
-export const defaultExcludeFiles = ['./__tests__/testData/examples/also_decoy.rs'];
-export const defaultExcludeFolders = ['./__tests__/testData/examples/decoy', './__tests__/testData/examples/excludefolder'];
+export const defaultExcludeFiles = ['./__tests__/testData/examples/this_file_should_be_excluded.rs'];
+export const defaultExcludeFolders = ['./__tests__/testData/examples/excludefolder', './__tests__/testData/examples/ios'];
 
 export const defaultTargets: ITarget[] = [
     { Path: './__tests__/testData/examples/README.md', Style: LinkStyle.Markdown },
