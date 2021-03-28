4c67c382c54cac2044994b0a58eca4c54821ccc4
Andreas Weibye <13300393+Weibye@users.noreply.github.com>
2021-03-28 11:00:03 +0200

    Rename

--- src/Config.ts
+++ src/Config.ts
@@ -2,17 +2,16 @@ import { ITarget } from './Interfaces';
 
 export class Config {
     public Source: string;
-    public WhitelistFileTypes: string[];
+    public FileTypes: string[];
     public ExcludeFolders: string[];
     public ExcludeFiles: string[];
     public Targets: ITarget[];
 
     public constructor(source: string, whitelistFileTypes: string[], excludeFolders: string[], excludeFiles: string[], targets: ITarget[]) {
         this.Source = source;
-        this.WhitelistFileTypes = whitelistFileTypes;
+        this.FileTypes = whitelistFileTypes;
         this.ExcludeFolders = excludeFolders;
         this.ExcludeFiles = excludeFiles;
-
         this.Targets = targets;
     }
 
@@ -22,7 +21,7 @@ export class Config {
     public ToString(): string {
         let output = '';
         output += `\tSource: ${this.Source}\n`;
-        output += `\tFileTypes: ${this.WhitelistFileTypes}\n`;
+        output += `\tFileTypes: ${this.FileTypes}\n`;
         output += `\tExcludeFolders: ${this.ExcludeFolders}\n`;
         output += `\tExcludeFiles: ${this.ExcludeFiles}\n`;
         for (const target of this.Targets) {
--- src/InclusionController.ts
+++ src/InclusionController.ts
@@ -2,7 +2,7 @@ import { Config } from './Config';
 import { FileDetails } from './FileDetails';
 
 export function IncludeFile(fileDetails: FileDetails, config: Config): boolean {
-    return !ExcludeFile(fileDetails.SourcePath, config.ExcludeFiles, config.ExcludeFolders) && WhitelistedType(fileDetails.Extension, config.WhitelistFileTypes);
+    return !ExcludeFile(fileDetails.SourcePath, config.ExcludeFiles, config.ExcludeFolders) && WhitelistedType(fileDetails.Extension, config.FileTypes);
 }
 
 export function IncludeFolder(path: string, config: Config): boolean {
