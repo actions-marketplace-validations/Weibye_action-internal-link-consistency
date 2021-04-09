38997b2c5b6f6fb7b1929f1afff7413908d95eea
Andreas Weibye <13300393+Weibye@users.noreply.github.com>
2021-04-09 21:32:14 +0200

    Adding source data tests

--- __tests__/SourceDataCollector.test.ts
+++ __tests__/SourceDataCollector.test.ts
@@ -1,19 +1,9 @@
-/*
- * - Should be able to read from a file
- * - Should not throw error on empty file
- * - Should fail if the file-type isn't supported yet
- * - Should correctly ignore files
- * - Should correctly ignore folders and files inside those folders
- * - [./root/path] and [root/path] should both be valid path input
- *
- */
-
 import { Config } from '../src/Config';
-import { ITarget } from '../src/Interfaces';
 import { SourceDataCollector } from '../src/DataCollection/SourceData';
 
 const sourcePath = './__tests__/validData/data/';
 
+// Source Path
 test('Should be able to collect files from a valid path', async () => {
     const config = new Config(sourcePath, []);
     const data = new SourceDataCollector(config);
@@ -23,6 +13,14 @@ test('Should be able to collect files from a valid path', async () => {
     expect(data.FileDetails.length).toBeGreaterThan(0);
 });
 
+test('Should not find anything in an empty folder', async () => {
+    const data = new SourceDataCollector(new Config('./__tests__/validData/data/emptyfolder/', []));
+    expect(data).toBeDefined();
+    expect(data.FileDetails).toBeDefined();
+    expect(data.FileDetails.length).toEqual(0);
+});
+
+// File Types
 test('Should collect everything on empty file-types', async () => {
     const fileTypes: string[] = [];
     const config = new Config(sourcePath, [], fileTypes);
@@ -53,6 +51,20 @@ test('Should only collect defined file-types if defined', async () => {
     }
 });
 
+test('Should not pick up files without extension if extension is defined', async () => {
+    const data = new SourceDataCollector(new Config(sourcePath, [], ['test']));
+    expect(data).toBeDefined();
+    expect(data.FileDetails).toBeDefined();
+    expect(data.FileDetails.length).toBeGreaterThan(0);
+
+    for (const fileDetails of data.FileDetails) {
+        expect(fileDetails.Extension).toBeDefined();
+        expect(fileDetails.Extension === '').toBeFalsy();
+        expect(fileDetails.FileName === 'NoFileExtension').toBeFalsy();
+    }
+});
+
+// Ignore files
 test('Should correctly ignore files', async () => {
     const ignoreFilesEmpty: string[] = [];
     const ignoreFileSingle: string[] = ['./__tests__/validData/data/should_be_ignored.sample'];
@@ -90,9 +102,36 @@ test('Should correctly ignore files', async () => {
     }
 });
 
+// Ignore folders
 test('Should correctly ignore folders and files inside those folders', async () => {
-    const source = './__tests__/validData/data/';
-    const config = new Config(source, [], [], [], []);
+    const ignoreFoldersEmpty: string[] = [];
+    const ignoreFoldersSingle: string[] = ['./__tests__/validData/data/ignorefolder'];
+    const ignoreFoldersMultiple: string[] = ['./__tests__/validData/data/ignorefolder', './__tests__/validData/data/subfolderOne'];
 
-    const data = new SourceDataCollector(config);
+    const noIgnore = new SourceDataCollector(new Config(sourcePath, [], [], ignoreFoldersEmpty));
+    expect(noIgnore).toBeDefined();
+    expect(noIgnore.FileDetails).toBeDefined();
+    expect(noIgnore.FileDetails.length).toBeGreaterThan(0);
+
+    const ignoreSingle = new SourceDataCollector(new Config(sourcePath, [], [], ignoreFoldersSingle));
+    expect(ignoreSingle).toBeDefined();
+    expect(ignoreSingle.FileDetails).toBeDefined();
+    expect(ignoreSingle.FileDetails.length).toBeGreaterThan(0);
+    expect(ignoreSingle.FileDetails.length).toBeLessThan(noIgnore.FileDetails.length);
+
+    // The ignored files should NOT exist in the list of files
+    for (const fileDetail of ignoreSingle.FileDetails) {
+        expect(ignoreFoldersSingle.some(e => fileDetail.SourcePath.includes(e))).toBeFalsy();
+    }
+
+    const ignoreMultiple = new SourceDataCollector(new Config(sourcePath, [], [], ignoreFoldersMultiple));
+    expect(ignoreMultiple).toBeDefined();
+    expect(ignoreMultiple.FileDetails).toBeDefined();
+    expect(ignoreMultiple.FileDetails.length).toBeGreaterThan(0);
+    expect(ignoreMultiple.FileDetails.length).toBeLessThan(ignoreSingle.FileDetails.length);
+
+    // The ignored files should NOT exist in the list of files
+    for (const fileDetail of ignoreMultiple.FileDetails) {
+        expect(ignoreFoldersMultiple.some(e => fileDetail.SourcePath.includes(e))).toBeFalsy();
+    }
 });
